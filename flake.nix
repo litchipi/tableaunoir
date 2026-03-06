934a31533f84014fc58aae4027c6f63975f3c780
Litchi Pi <litchi.pi@proton.me>
2026-03-06 13:06:17 +0100

    Fixup CI
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs: with inputs; flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs { inherit system; };
+
+    mkScript = name: text: let
+      app = pkgs.writeShellApplication {
+        inherit name text;
+        runtimeInputs = [
+          pkgs.nodejs
+          pkgs.ripgrep
+          pkgs.gnused
+          pkgs.findutils
+        ];
+      };
+    in { type = "app"; program = "${app}/bin/${name}"; };
+  in {
+    apps.install = mkScript "install-node-deps" ''
+      npm install
+      rg -n "usr/bin/env node" -l node_modules/ | xargs sed -i "s+/usr/bin/env node+${pkgs.nodejs}/bin/node+g"
+      rg -n "usr/bin/env bash" -l node_modules/ | xargs sed -i "s+/usr/bin/env bash+${pkgs.bash}/bin/bash+g"
+      rg -n "usr/bin/env sh" -l node_modules/ | xargs sed -i "s+/usr/bin/env sh+${pkgs.bash}/bin/bash+g"
+    '';
+
+    apps.build = mkScript "build-pkg" ''
+      export NODE_ENV=production
+      npm run build
+    '';
+
+    apps.lint = mkScript "lint-pkg" ''
+      npm run lint | tee dist/status.txt
+    '';
+  });
+}
